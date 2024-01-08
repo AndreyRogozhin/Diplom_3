b3a9ca8d47dc799de5a03c8febe456d27254e92d
Andrey Rogozhin <andrey.rogozhin.2023@yandex.ru>
2024-01-09 00:40:57 +0300

    Тест страницы регистрации, тест страницы восстановления пароля ( переход на страницу логина )

--- src/test/java/org/example/TestRecoverPasswordPage.java
+++ src/test/java/org/example/TestRecoverPasswordPage.java
@@ -20,11 +20,6 @@ public class TestRecoverPasswordPage {
     @Test
     public void runTestRecoverPasswordPageOK()  {
 
-
-//        driver.get(registerPageUrl);
-
-
-//        MainPage objMainPage = new MainPage(browserRule.getWebDriver());
         RecoverPasswordPage objRecoverPasswordPage = new RecoverPasswordPage(browserRule.getWebDriver());
         LoginPage objLoginPage = new LoginPage(browserRule.getWebDriver());
         objRecoverPasswordPage.openRecoverPasswordPage(recoverPasswordPageUrl);
@@ -36,34 +31,5 @@ public class TestRecoverPasswordPage {
         // дождаться открытия страницы
         // проверить, что открылась страница с первой формой для заполнения
         Assert.assertTrue("Не открылась страница авторизации",objLoginPage.checkLoginPageShown (loginPageUrl));
-
     }
-
-    @Test
-    public void runTestRegisterPageBadPassword()  {
-
-//        MainPage objMainPage = new MainPage(browserRule.getWebDriver());
-        RegisterPage objRegisterPage = new RegisterPage(browserRule.getWebDriver());
-        LoginPage objLoginPage = new LoginPage(browserRule.getWebDriver());
-        objRegisterPage.openRegisterPage(registerPageUrl);
-
-        // проверить переход по нажатию на кнопку Зарегистрироваться
-
-        String registerPageUrl = Url.REGISTER_PAGE;
-
-        // нужно придумать случайное имя пользователя, почту и пароль. Пароль - короткий, менее 6 символов
-        objRegisterPage.setEmail("www2@yandex.ru");
-        objRegisterPage.setName("fff");
-        objRegisterPage.setPassword("fff");
-
-        objRegisterPage.clickRegisterButton();
-        // дождаться открытия страницы
-        // проверить, что открылась страница с первой формой для заполнения
-        Assert.assertTrue("Нет сообщения об ошибке!",objRegisterPage.passwordErrorMessageFound());
-
-    }
-
-
-
-
 }
